7bb311367fdab4a6636f21456ce08047cc589386
albert <albertbenny2701@example.com>
2024-04-28 20:42:45 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace WindowsFormsAppcrud1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2_show = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2_search = new System.Windows.Forms.Button();
+            this.button2_delete = new System.Windows.Forms.Button();
+            this.button3_update = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(288, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "age";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(369, 190);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(369, 228);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(197, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(369, 273);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(197, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(285, 278);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "phone";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(491, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(113, 410);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(699, 206);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button2_show
+            // 
+            this.button2_show.Location = new System.Drawing.Point(616, 342);
+            this.button2_show.Name = "button2_show";
+            this.button2_show.Size = new System.Drawing.Size(75, 23);
+            this.button2_show.TabIndex = 8;
+            this.button2_show.Text = "show";
+            this.button2_show.UseVisualStyleBackColor = true;
+            this.button2_show.Click += new System.EventHandler(this.button2_show_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(369, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(239, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(285, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "student";
+            // 
+            // button2_search
+            // 
+            this.button2_search.Location = new System.Drawing.Point(648, 57);
+            this.button2_search.Name = "button2_search";
+            this.button2_search.Size = new System.Drawing.Size(75, 23);
+            this.button2_search.TabIndex = 11;
+            this.button2_search.Text = "search";
+            this.button2_search.UseVisualStyleBackColor = true;
+            this.button2_search.Click += new System.EventHandler(this.button2_search_Click);
+            // 
+            // button2_delete
+            // 
+            this.button2_delete.Location = new System.Drawing.Point(288, 342);
+            this.button2_delete.Name = "button2_delete";
+            this.button2_delete.Size = new System.Drawing.Size(75, 23);
+            this.button2_delete.TabIndex = 12;
+            this.button2_delete.Text = "delete";
+            this.button2_delete.UseVisualStyleBackColor = true;
+            this.button2_delete.Click += new System.EventHandler(this.button2_delete_Click);
+            // 
+            // button3_update
+            // 
+            this.button3_update.Location = new System.Drawing.Point(397, 342);
+            this.button3_update.Name = "button3_update";
+            this.button3_update.Size = new System.Drawing.Size(75, 23);
+            this.button3_update.TabIndex = 13;
+            this.button3_update.Text = "update";
+            this.button3_update.UseVisualStyleBackColor = true;
+            this.button3_update.Click += new System.EventHandler(this.button3_update_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 681);
+            this.Controls.Add(this.button3_update);
+            this.Controls.Add(this.button2_delete);
+            this.Controls.Add(this.button2_search);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2_show);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2_show;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2_search;
+        private System.Windows.Forms.Button button2_delete;
+        private System.Windows.Forms.Button button3_update;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsAppcrud1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-BMLIOU8H\SQLEXPRESS;Initial Catalog=student2;Integrated Security=True");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string show = "select * from crud";
+                SqlDataAdapter sda = new SqlDataAdapter(show, con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            try
+            {
+                con.Open();
+                DataSet ds = new DataSet();
+                string u_name = "select * from crud";
+                SqlDataAdapter sda = new SqlDataAdapter(u_name, con);
+                sda.Fill(ds);
+                comboBox1.DataSource = ds.Tables[0];
+                comboBox1.DisplayMember = ds.Tables[0].Columns[0].ToString();
+                con.Close();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("ivalid" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string querry = "insert into crud(name,age,phone)values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, con);
+                DataTable dt = new DataTable();
+                sda.SelectCommand.ExecuteNonQuery();
+                MessageBox.Show("success");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            try
+            {
+                con.Open();
+                string show = "select * from crud";
+                SqlDataAdapter sda = new SqlDataAdapter(show, con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button2_show_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string show = "select * from crud";
+                SqlDataAdapter sda = new SqlDataAdapter(show, con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button2_search_Click(object sender, EventArgs e)
+        {
+            string s_name = comboBox1.Text;
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Select * from [crud] where (name='" + s_name + "')", con);
+                SqlDataReader ab;
+                ab = cmd.ExecuteReader();
+                while (ab.Read())
+                {
+                    textBox1.Text = ab.GetValue(0).ToString();
+                    textBox2.Text = ab.GetValue(1).ToString();
+                    textBox3.Text = ab.GetValue(2).ToString();
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("not found" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+
+        }
+
+        private void button3_update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string querry = "update crud set age='" + textBox2.Text + "',phone='" + textBox3.Text + "' where name='" + textBox1.Text + "'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, con);
+                sda.SelectCommand.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("update successfully");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            try
+            {
+                con.Open();
+                string show = "select * from crud";
+                SqlDataAdapter sda = new SqlDataAdapter(show, con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void button2_delete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand dlt = new SqlCommand("delete from crud where name='" + comboBox1.Text + "'",con);
+                dlt.ExecuteNonQuery();
+                MessageBox.Show("successfully deleted");
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            try
+            {
+                con.Open();
+                string show = "select * from crud";
+                SqlDataAdapter sda = new SqlDataAdapter(show, con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+            try
+            {
+                con.Open();
+                string show = "select * from crud";
+                SqlDataAdapter sda = new SqlDataAdapter(show, con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                dataGridView1.DataSource = dt;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
